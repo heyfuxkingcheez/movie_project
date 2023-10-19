df93c6f67123bf3d542f666958bcdd8272e1fe65
Your Name <you@example.com>
2023-10-19 20:02:10 +0900

    [feat] 모달창 클릭이벤트

--- movie.js
+++ movie.js
@@ -53,7 +53,7 @@ for (let i = 0; i < 5; i++) {
         <div class="modal-dialog modal-dialog-centered modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">ID : ${movieId}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
