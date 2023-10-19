5de5dba1aa669554d0743cb3bf6b655b28ee678d
Your Name <you@example.com>
2023-10-20 03:59:06 +0900

    [feat] 검색기능 추가

--- movie.js
+++ movie.js
@@ -7,14 +7,18 @@ const options = {
             'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzc4YmI0ZTA3ZGM2ZmM5NTIwYjg2OGU2OTQ0MjA5MSIsInN1YiI6IjY1MmY3MTEwYzk5NWVlMDEwMGRlOWEzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6feX7u20gdSOrsez2RlWSmZyIfdyK-tkrj6YwN2JHIk'
     }
 };
+let searchArr;
+let movieMap = new Map();
+
 for (let i = 0; i < 5; i++) {
-    fetch(`https://api.themoviedb.org/3/movie/popular?language=ko-kr&page=${i}`,
+    fetch(`https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${i}`,
         options)
         .then(response => response.json())
         .then(response => {
             let movieResult = response.results;
             let movieArr = [...movieResult];
-            console.log("api불러오기 성공!", movieArr);
+            searchArr = response['results'];
+            console.log("api불러오기 성공!", searchArr);
 
             movieArr.forEach((movie, i) => {
                 let temp = document.createElement("div");
@@ -26,15 +30,43 @@ for (let i = 0; i < 5; i++) {
                 let movieRating = movieArr[i].vote_average;
                 let movieDate = movieArr[i].release_date;
                 let moviePoster1 = movieArr[i].poster_path
-                let strposter_path = moviePoster1.substr(6, 10);
-                let strposter_path1 = strposter_path.replace('0', '').replace('1', '').replace('2', '').replace('3', '').replace('4', '').replace('5', '').replace('6', '').replace('7', '').replace('8', '').replace('9', '');
+                let strposter_path = moviePoster1.substr(2, 10);
+                let strposter_path1 = strposter_path.replace('1', '').replace('2', '').replace('3', '').replace('4', '').replace('5', '').replace('6', '').replace('7', '').replace('8', '').replace('9', '').replace('0', '');
 
                 console.log("코드보면 웃음 밖에 안나옴ㅋ =>", strposter_path1)
 
+                // 제목 글자 ...로 줄이기
+                let limitLength = 25;
+                if (movieTitle.length > limitLength) {
+                    movieTitle = movieTitle.substr(0, limitLength - 2) + '...';
+                }
 
+                // 검색 기능
+
+                const searchBtn = document.querySelector('.searchBtn');
+                const searchInput = document.querySelector('.searchInput');
+
+                function search() {
+                    const searchText = searchInput.value;
+                    searchArr.forEach((a) => {
+                        let movieTitle = movieArr[i].title;
+                        let check = movieTitle.includes(searchText);
+                        movieMap.set(movieTitle, temp);
+                        if (check) {
+                            movieMap.get(movieTitle).style.display = 'block';
+                        } else {
+                            movieMap.get(movieTitle).style.display = 'none';
+                        }
+                    })
+                }
+                // 검색 클릭
+                searchBtn.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    search();
+                });
 
                 temp.innerHTML = `
-            <div class="movie-card" data-bs-toggle="modal" data-bs-target="#${strposter_path1}">
+            <div id="wook" class="movie-card" data-bs-toggle="modal" data-bs-target="#${strposter_path1}">
             <img src=${moviePoster}
                 alt="poster image"
             />
@@ -66,22 +98,12 @@ for (let i = 0; i < 5; i++) {
             </div>
         </div>
     </div>`;
-                document.querySelector(".movie-grid").append(temp);
+                document.querySelector(".movie-grid").appendChild(temp);
 
-                //클릭 이벤트
-                const clickcard = document.querySelector('temp')
-
-                temp.addEventListener("click", (e) => {
-                    console.log(`해당 영화의 id => ${movieId}`);
-                });
+                
             })
         })
         .catch(error => console.log('ERROR'));
 }
-
-
-
-
-
 
 
