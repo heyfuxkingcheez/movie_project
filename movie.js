5ad79dfe7e123032a050afcff8cb0840e870c076
Your Name <you@example.com>
2023-10-20 16:42:25 +0900

    [feat] 주석 추가

--- movie.js
+++ movie.js
@@ -1,4 +1,5 @@
 
+// HTTP 요청에 대한 옵션을 정의합니다.
 const options = {
     method: 'GET',
     headers: {
@@ -7,22 +8,29 @@ const options = {
             'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzc4YmI0ZTA3ZGM2ZmM5NTIwYjg2OGU2OTQ0MjA5MSIsInN1YiI6IjY1MmY3MTEwYzk5NWVlMDEwMGRlOWEzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6feX7u20gdSOrsez2RlWSmZyIfdyK-tkrj6YwN2JHIk'
     }
 };
+// 영화 데이터를 저장할 변수를 선언합니다.
 let searchArr;
 let movieMap = new Map();
 
+// 반복문을 사용하여 영화 데이터를 가져옵니다.
 for (let i = 0; i < 5; i++) {
     fetch(`https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${i}`,
         options)
         .then(response => response.json())
         .then(response => {
+            // API 응답에서 영화 결과를 추출합니다
             let movieResult = response.results;
             let movieArr = [...movieResult];
             searchArr = response['results'];
             console.log("api불러오기 성공!", searchArr);
 
+            // 각 영화에 대한 HTML 요소를 생성합니다.
             movieArr.forEach((movie, i) => {
+                // 각 영화를 위한 div 요소를 생성합니다.
                 let temp = document.createElement("div");
                 temp.setAttribute("class", "poster");
+
+                // API 응답에서 영화 정보를 추출합니다.
                 let movieTitle = movieArr[i].title;
                 let movieOverview = movieArr[i].overview;
                 let moviePoster = `https://image.tmdb.org/t/p/w500${movieArr[i].poster_path}`;
@@ -32,17 +40,18 @@ for (let i = 0; i < 5; i++) {
                 let moviePoster1 = movieArr[i].poster_path
                 let strposter_path = moviePoster1.substr(2, 10);
                 let strposter_path1 = strposter_path.replace('1', '').replace('2', '').replace('3', '').replace('4', '').replace('5', '').replace('6', '').replace('7', '').replace('8', '').replace('9', '').replace('0', '');
-
                 console.log("코드보면 웃음 밖에 안나옴ㅋ =>", strposter_path1)
 
-                // 제목 글자 ...로 줄이기
+                // 제목 길이 ...로 줄이기
                 let limitLength = 25;
                 if (movieTitle.length > limitLength) {
                     movieTitle = movieTitle.substr(0, limitLength - 2) + '...';
                 }
 
-                // 검색 기능
+                // 검색 입력 요소에 포커스를 맞춥니다.
                 document.querySelector('.searchInput').focus();
+
+                // 영화 검색을 수행하는 함수
                 const searchBtn = document.querySelector('.searchBtn');
                 const searchInput = document.querySelector('.searchInput');
 
@@ -59,7 +68,7 @@ for (let i = 0; i < 5; i++) {
                         }
                     })
                 }
-                // 검색 클릭
+                // 검색 버튼에 클릭 이벤트 리스너를 추가합니다.
                 searchBtn.addEventListener("click", (e) => {
                     e.preventDefault();
                     search();
@@ -72,6 +81,7 @@ for (let i = 0; i < 5; i++) {
                 //     }
                 // })
 
+                // 각 영화에 대한 HTML 내용을 생성합니다.
                 temp.innerHTML = `
             <div id="wook" class="movie-card" data-bs-toggle="modal" data-bs-target="#${strposter_path1}">
             <img src=${moviePoster}
@@ -105,6 +115,7 @@ for (let i = 0; i < 5; i++) {
             </div>
         </div>
     </div>`;
+                // 영화 요소를 영화 그리드에 추가합니다.
                 document.querySelector(".movie-grid").appendChild(temp);
 
 
